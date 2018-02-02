5f29b4731d08dadf83e265fa043d7255eb382047
zhiwen.zuo <zhiwen.zuo@aihuishou.com>
2018-02-02 15:46:42 +0800

    添加项目文件。

---
+++ QuartzTestWeb/Extensions/QuartzExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Impl.Calendar;
+using Quartz.Spi;
+
+namespace Quartz
+{
+    public static class QuartzExtension
+    {
+        /// <summary>
+        /// 启用Quartz框架的Job
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        public static void AddScheduling(this IServiceCollection services, IConfiguration configuration)
+        {
+            ISchedulerFactory sf = new StdSchedulerFactory();
+            IScheduler scheduler = sf.GetScheduler().GetAwaiter().GetResult();
+
+            var implementTypes = Assembly.GetEntryAssembly().GetTypes().Where(i => i.GetTypeInfo().ImplementedInterfaces.Contains(typeof(IJob)));
+            foreach (var implementType in implementTypes)
+            {
+                var jobDetail = JobBuilder.Create(implementType).WithIdentity(implementType.Name).Build();
+                var trigger = TriggerBuilder.Create().WithCronSchedule(configuration[$"Schueduling:{implementType.Name}"]).Build();
+                scheduler.ScheduleJob(jobDetail, trigger);
+            }
+
+            scheduler.Start();
+
+            services.AddSingleton<ISchedulerFactory>(sf);
+            services.AddSingleton(scheduler);
+        }
+    }
+}
---
+++ QuartzTestWeb/Jobs/TestJob1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace QuartzTestWeb.Jobs
+{
+    public class TestJob1 : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            System.Diagnostics.Debug.WriteLine($"{this.GetType().Name} executing...");
+        }
+    }
+}
